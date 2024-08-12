a34be566ecb58fe6fe9033df65efd460d0c0f59d
4hoeschele <Carsten Hoeschele>
2024-08-12 12:02:29 +0200

    [main IMPROVE] Removed Email communication from portfolio

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -5,51 +5,46 @@ import { ReactComponent as LinkedInIcon } from "../../assets/img/logo--linkedin.
 import { ReactComponent as XingIcon } from "../../assets/img/logo--xing.svg";
 
 const Contact = () => {
-  const socials = [
-    {
-      name: "LinkedIn",
-      Icon: LinkedInIcon,
-      url: "https://www.linkedin.com/in/carsten-h%C3%B6schele-958882270/",
-    },
-    {
-      name: "Xing",
-      Icon: XingIcon,
-      url: "https://www.xing.com/profile/Carsten_Hoeschele2/web_profiles?expandNeffi=true",
-    },
-    {
-      name: "Email",
-      Icon: EmailIcon,
-      url: "mailto:hcarsten1@googlemail.com",
-    },
-  ];
-  return (
-    <div className="contactWrapper" id="contact">
-      <section className="contact">
-        <div className="contact__description">
-          <h2>Contact</h2>
-          <p className="contactTxt">
-            I am currently looking for new opportunities and would love to hear
-            from you. Please feel free to reach out.
-          </p>
-          {/*socials*/}
-          <div className="social-links">
-            {socials.map((social, index) => (
-              <a
-                key={index}
-                href={social.url}
-                className="social-link"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <social.Icon className="social-icon" />
-                <span className="sr-only">{social.name}</span>
-              </a>
-            ))}
-          </div>
+    const socials = [
+        {
+            name: "LinkedIn",
+            Icon: LinkedInIcon,
+            url: "https://www.linkedin.com/in/carsten-h%C3%B6schele-958882270/",
+        },
+        {
+            name: "Xing",
+            Icon: XingIcon,
+            url: "https://www.xing.com/profile/Carsten_Hoeschele2/web_profiles?expandNeffi=true",
+        },
+    ];
+    return (
+        <div className="contactWrapper" id="contact">
+            <section className="contact">
+                <div className="contact__description">
+                    <h2>Contact</h2>
+                    <p className="contactTxt">
+                        I am currently looking for new opportunities and would love to hear
+                        from you. Please feel free to reach out.
+                    </p>
+                    {/*socials*/}
+                    <div className="social-links">
+                        {socials.map((social, index) => (
+                            <a
+                                key={index}
+                                href={social.url}
+                                className="social-link"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <social.Icon className="social-icon" />
+                                <span className="sr-only">{social.name}</span>
+                            </a>
+                        ))}
+                    </div>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
+    );
 };
 
 export default Contact;
